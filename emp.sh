b9a71af399549f8dc80974ca25d4fe839db657b9
DivyaMishra6 <divyamishra599@gmail.com>
2023-02-25 00:05:52 +0530

    [Divya] Add ; Shell script program to check Emplyoee Salary FullTime and PartTime using Switch Case

--- emp.sh
+++ emp.sh
@@ -1,12 +1,32 @@
-#!/bin/bash
+#!/bin/bash 
 
-echo "Checking Employee is present or absent"
+# 0/1/2
+random_check=$(( RANDOM % 3 ))
 
-res=$(( RANDOM % 2 ))
+is_present_full_time=1
+is_present_part_time=2
+wage_per_hour=20
 
-if [ $res -eq 1 ]
-then
-	echo "Employee is present"
-else
-	echo "Employee is absent"
-fi
+
+case $random_check in
+
+$is_present_full_time) emp_hours=8
+                    #    salary=$(( $emp_hours * $wage_per_hour))
+                       echo " Employee is present for full time"
+                    #    echo "Salary of the full time employee is $salary"
+                       ;;
+$is_present_part_time) emp_hours=4
+                    #    salary=$(( $emp_hours * $wage_per_hour))
+                       echo " Employee is present for part time"
+                    #    echo "Salary of the part time employee is $salary"
+                       ;;
+*) emp_hours=0
+#    salary=$(( $emp_hours * $wage_per_hour)) 
+   echo " Employee is absent"
+   ;;
+
+esac
+
+salary=$(( $emp_hours * $wage_per_hour))
+echo " Salary of an employee is $salary "
+
