b465e08a2b969f17ff0f5ce2093c7f437a5fec7e
DivyaMishra6 <divyamishra599@gmail.com>
2023-02-24 14:33:47 +0530

    [Divya] Add ; Shell script program to check whether the Employee is working Fulltime or PartTime

--- emp.sh
+++ emp.sh
@@ -1,12 +1,26 @@
-#!/bin/bash
+#!/bin/bash -x 
 
-echo "Checking Employee is present or absent"
+# let the present emp is denote the 1
+empPerHrSalary=20
+# get the random number using RANDOM function
+result=$((RANDOM%3))
 
-res=$(( RANDOM % 2 ))
-
-if [ $res -eq 1 ]
+# check the condition and calculate the salary employee is present the daily wage or not 
+if [ $result -eq 0 ]
 then
-	echo "Employee is present"
-else
-	echo "Employee is absent"
+       echo "Employee is Absent"
+     empHrs=0
+
+elif [ $result  -eq  1 ]
+then   
+          echo "Employee is working FullTime"
+       empHrs=8
+  
+# emp is absent the daily wage then display 
+else   
+       echo "Employee is working PartTime"
+       empHrs=4
 fi
+
+salary=$(( $empHrs * $empPerHrSalary ))
+echo "Total Employee Salary is : $salary"
