8a178b00d676e7dfc775155b4e5be16db6916b8b
DivyaMishra6 <divyamishra599@gmail.com>
2023-02-27 20:44:23 +0530

    [Divya] Add ; shell Script program to calculate Employee wage and refactor code

--- emp.sh
+++ emp.sh
@@ -1,40 +1,52 @@
-                                                 UC5
- #!/bin/bash
+#! /bin/bash -x
 
-#Constant variables
+#constant variable
 is_present_full_time=1
 is_present_part_time=2
 wage_per_hour=20
+max_work_hour=100
 total_working_days=20
 
-#Variable
+#variable
 total_work_hour=0
+day=1
 
-#Iterating FOR loop for 20 times, because total working days is 20
-for ((day=1 ;day<=$total_working_days; day++ ))
-do
-# 0/1/2
-random_check=$(( RANDOM % 3 ))
+# function to get the work hours of the employee
+function get_work_hours()
+{
+local random_check=$1
+local emp_hours=0
+
+#selection
 case $random_check in
-
-$is_present_full_time) emp_hours=8
-echo " Employee is present for full time"
+$is_present_full_time)
+emp_hours=8
 ;;
-
-$is_present_part_time) emp_hours=4
-echo " Employee is present for part time"
+$is_present_part_time)
+emp_hours=4
 ;;
-*) emp_hours=0
-echo " Employee is absent"
+*)
+emp_hours=0
 ;;
 
 esac
+echo $emp_hours
+}
 
-salary=$(( $emp_hours * $wage_per_hour))
-total_work_hour=$(( $total_work_hour + $emp_hours))
-echo " Salary of an employee is $salary "
+
+#Loop will execute till both the condition is TRUE
+while (($day <= $total_working_days && $total_work_hour <= $max_work_hour ))
+do
+#variable
+random_check=$((RANDOM%3)) # 0/1/2
+
+#calling the function and passing the argument
+emp_hours=$(get_work_hours $random_check)
+
+total_work_hour=$(($total_work_hour + $emp_hours))
+((day++))
+
 done
 
-# Calculating total salary for 20 days on the basis of employee hours
-total_salary=$(( $total_work_hour * $wage_per_hour))
-echo " Total Salary of an Employee is $total_salary "
+total_salary=$(( total_work_hour * wage_per_hour))
+echo "Total Salary of an Employee is $total_salary"
